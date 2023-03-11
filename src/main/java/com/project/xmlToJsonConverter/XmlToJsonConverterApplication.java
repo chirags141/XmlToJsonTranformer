b29d3062659835d46fc0590e7e57acce70ff8e6f
Chirag Sethiya <chiragsethiya123@gmail.com>
2023-03-11 11:53:02 +0530

    Initial Commit

---
+++ src/main/java/com/project/xmlToJsonConverter/XmlToJsonConverterApplication.java
@@ -0,0 +1,13 @@
+package com.project.xmlToJsonConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlToJsonConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlToJsonConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/xmlToJsonConverter/XmlToJsonConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.xmlToJsonConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlToJsonConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
